81042895fc9c387a28f44e941b745528641d93f3
anil sahu <anil94sahu@gmail.com>
2023-08-31 18:06:35 +0530

    changes done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/sample-nextjs-project',
+    assetPrefix: '/sample-nextjs-project'
 }
 
 module.exports = nextConfig

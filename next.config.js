0c554b492664528e5b6955a857f05e00640bbfe9
anil sahu <anil94sahu@gmail.com>
2023-08-31 18:49:09 +0530

    remove base path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/sample-nextjs-project',
-    assetPrefix: '/sample-nextjs-project'
+    basePath: '/',
+    assetPrefix: '/'
 }
 
 module.exports = nextConfig

60b24c158bce0b9bde79f861ce29ed1779fd907d
anil sahu <anil94sahu@gmail.com>
2023-08-31 16:37:46 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  image: {
-    loader : 'akamai',
-    path: ""
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig

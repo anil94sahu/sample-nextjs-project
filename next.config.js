cee468b3aeb906f8e8e1d3d8d842c6fb011a8841
anil sahu <anil94sahu@gmail.com>
2023-09-02 14:43:59 +0530

    comment prefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/sample-nextjs-project'
+    // assetPrefix: '/sample-nextjs-project'
 }
 
 module.exports = nextConfig
